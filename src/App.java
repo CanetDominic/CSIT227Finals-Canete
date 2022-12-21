26701778fcf902e381ab86060b56c7d4d2fe2e54
DominicQT <dominiccanete75@gmail.com>
2022-12-21 10:49:31 +0800

    MILESTONE A: Show the GUI

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class App extends JFrame{
     private JPanel pnlMain;
     private JRadioButton rbCustomer;
     private JRadioButton rbClerk;
@@ -20,6 +20,7 @@ public class App {
     private JButton btnSavePerson;
     private JButton btnLoadPerson;
     private JButton btnReward;
+    private JPanel Person;
 
     private List<Person> persons;
 
@@ -30,6 +31,11 @@ public class App {
 
     public static void main(String[] args) {
         // add here how to make GUI visible
+        JFrame frame = new JFrame("Person App");
+        frame.setContentPane(new App().Person);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700, 700);
+        frame.setVisible(true);
     }
 
     static void giveReward(int n) {
